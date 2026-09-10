347b3250ffbff6f41193b54de42a266d2443e0e0
Zeno-dav <growzenosmm@gmail.com>
2026-09-10 14:31:43 +0530

    Update cron.js

--- api/cron.js
+++ api/cron.js
@@ -8,7 +8,8 @@ export default async function handler(req, res) {
       }
     });
 
-    return res.status(200).json({ success: true, status: response.status });
+    const body = await response.text();
+    return res.status(200).send(body);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
